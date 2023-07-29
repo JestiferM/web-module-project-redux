0e52100db9f218e9e0d2fe7cb6f5fcac895f152f
JestiferM <jestifermartinez01@gmail.com>
2023-07-28 21:40:09 -0700

    almost done with module

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -9,7 +9,7 @@ const Movie = (props) => {
     const { id } = useParams();
     const { push } = useHistory();
 
-    const { movies, deleteMovie } = props;
+    const { movies, displayFavorties, deleteMovie } = props;
     const movie = movies.find(movie=>movie.id===Number(id));
     
     const handleDeleteClick = () => {
@@ -58,7 +58,8 @@ const Movie = (props) => {
 
 const mapStateToProps = (state) =>{
     return ({
-        movies:state.movies
+        displayFavorties: state.favortiesReducer.displayFavorties,
+        movies:state.movieReducer.movies
     });
 }
 
